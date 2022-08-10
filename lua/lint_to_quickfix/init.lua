bce9ac2c292b462af27613d3170e256815eddf48
Moritz Sauter <sautermoritz@gmx.de>
2022-08-10 22:48:27 +0200

    feat: Export the full lint function
    Export it again to give a consistent interface to the outside. Also
    adding a little bit of further typing documentation.

--- lua/lint_to_quickfix/init.lua
+++ lua/lint_to_quickfix/init.lua
@@ -1,3 +1,12 @@
+---@mod lint_to_quickfix
+---@brief [[
+---This plugin gives you the possiblity to easily run a linter
+---and get the results into your quickfix list.
+---
+---Currently it supports mainly some linters for python and bash/sh
+---but new ones can be added quite easily.
+--=@brief ]]
+
 ---@alias LintToQfLinter
 ---| 'pylint' # lint with pylint
 ---| 'mypy' # lint with mypy
@@ -7,9 +16,11 @@
 ---| 'a' # found items are added to qflist
 ---| 'r' # found items replace qflist
 
+---@alias CurriedLinterFunction fun(file_path?: string, mode?: LintToQfMode) The linter functions curried with sensible defaults.
+
 ---@param linter LintToQfLinter
 ---@param file_types string[]
----@return fun(file_path?: string, mode?: LintToQfMode)
+---@return CurriedLinterFunction
 local function curry(linter, file_types)
     return function(file_path, mode)
         require("lint_to_quickfix.main").lint_to_qf(
@@ -21,8 +32,15 @@ local function curry(linter, file_types)
     end
 end
 
+---@class LinterToQf
+---@field pylint CurriedLinterFunction
+---@field mypy CurriedLinterFunction
+---@field shellcheck CurriedLinterFunction
+
+---@export LinterToQf
 return {
     pylint = curry("pylint", { "python" }),
     mypy = curry("mypy", { "python" }),
     shellcheck = curry("shellcheck", { "sh", "zsh", "bash" }),
+    lint = require("lint_to_quickfix.main").lint_to_qf,
 }
